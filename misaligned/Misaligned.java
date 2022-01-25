4820278ffb265822ed028974860a0f91a5c4d3d1
Vaishakh G M <mehendalevgm@gmail.com>
2022-01-25 18:28:43 +0530

    New tests added

---
+++ misaligned/Misaligned.java
@@ -0,0 +1,27 @@
+public class Misaligned {
+    static String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    static String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    static String colorMap = new String();
+    static String printColorMap() {
+
+        int i = 0, j = 0;
+        for(i = 0; i < 5; i++) {
+            for(j = 0; j < 5; j++) {
+                colorMap += i * 5 + j +" | "+majorColors[i]+" | "+minorColors[i]+"\n";
+                //System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+            }
+        }
+
+        System.out.println(colorMap);
+        return colorMap;
+    }
+    public static void main(String[] args) { 
+        String colorMap = printColorMap();
+        int result = colorMap.split("\n").length;
+
+        assert(result == 25);
+        assert("1 | White | Blue" == colorMap.split("\n")[0]);
+
+        System.out.println("All is well (maybe!)");
+    }
+}
