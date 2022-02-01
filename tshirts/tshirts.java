3bde1775aa572bad769b9a282460b1a55f58e91f
VaishakhGurumurthy-Mehendale <91051638+VaishakhGurumurthy-Mehendale@users.noreply.github.com>
2022-02-02 01:07:14 +0530

    Generalized Test Logic

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Tshirts {
   static String size(int cms) {
         if(cms > 21 && cms <= 28 ) {
@@ -11,17 +13,30 @@ public class Tshirts {
         }else if(cms > 42 && cms <=48 ){
             return "XL";
          }else{
-            return "Size not available";
+            return "Invalid";
         }
     }
 
-    public static void main(String[] args) { 
-        assert(size(22) == "XS");
-        assert(size(32) == "S");
-        assert(size(38) == "M");
-        assert(size(42) == "L");
-        assert(size(48) == "XL");
-        assert(size(49) == "Size not available");
+    public static void main(String[] args) {
+
+      Map<String, List<Integer>> sizeMap = new HashMap<String, List<Integer>>();
+      sizeMap.put("XS", Arrays.asList(22, 23, 24, 25, 26, 27, 28));
+      sizeMap.put("S", Arrays.asList(29, 30, 31, 32));
+      sizeMap.put("M", Arrays.asList(33, 34, 35, 36, 37, 38));
+      sizeMap.put("L", Arrays.asList(39, 40, 41, 42));
+      sizeMap.put("XL", Arrays.asList(43, 44, 45, 46, 47, 48));
+
+      //Test to cover all possible test values
+      for(int textSize=0; textSize<100; textSize++){
+
+          if(textSize<22 || textSize>48){
+              assert(size(textSize).equals("Invalid"));
+          }else {
+              assert (sizeMap.get(size(textSize)).contains(textSize));
+          }
+
+      }
+
         System.out.println("All is well (maybe!)");
     }
 }
