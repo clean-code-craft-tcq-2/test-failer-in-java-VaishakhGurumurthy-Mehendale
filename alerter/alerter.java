28cce32a7ff2c62896d6d613fe2536f4fb0798bd
VaishakhGurumurthy-Mehendale <91051638+VaishakhGurumurthy-Mehendale@users.noreply.github.com>
2022-02-02 01:08:56 +0530

    Test added to check all functions/methods.

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -1,28 +1,43 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Alerter {
-        static int alertFailureCount = 0;
+    static int alertFailureCount = 0;
+    static boolean failureFlag;
+
     static int networkAlertStub(float celcius) {
         System.out.println("ALERT: Temperature is " + celcius + " celcius");
-        // Return 200 for ok
-        // Return 500 for not-ok
-        // stub always succeeds and returns 200
-        return 200;
+        int random = new Random().nextInt(2);
+        // assuming random network response, return some value
+        return new int[]{200,500}[random];
     }
+
     static void alertInCelcius(float farenheit) {
         float celcius = (farenheit - 32) * 5 / 9;
         int returnCode = networkAlertStub(celcius);
         if (returnCode != 200) {
-            // non-ok response is not an error! Issues happen in life!
-            // let us keep a count of failures to report
-            // However, this code doesn't count failures!
-            // Add a test below to catch this bug. Alter the stub above, if needed.
-            alertFailureCount += 0;
+            failureFlag = true;
+            alertFailureCount += 1;
         }
     }
     public static void main(String[] args) {
+
+        // Random number of function calls to test if failureCounts are tracked properly
         alertInCelcius(400.5f);
         alertInCelcius(303.6f);
-        assert (alertFailureCount > 0);
+
+        if(failureFlag)
+            assert (alertFailureCount > 0);
+        else
+            assert (alertFailureCount == 0);
         System.out.printf("%d alerts failed.\n", alertFailureCount);
+
+
+
+        //Test to check if networkStud returns expected set of values
+        assert (Arrays.asList(200, 500).contains(networkAlertStub(100)));
+
         System.out.println("All is well (maybe!)\n");
     }
 }
