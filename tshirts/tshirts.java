2f55a7b41340da7980aedb231ff4e96892f29324
VaishakhGurumurthy-Mehendale <91051638+VaishakhGurumurthy-Mehendale@users.noreply.github.com>
2022-02-02 00:14:38 +0530

    Improved test coverage and logic coverage

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -1,19 +1,27 @@
 public class Tshirts {
   static String size(int cms) {
-        if(cms < 38) {
+        if(cms > 21 && cms <= 28 ) {
+            return "XS";
+        } else if(cms > 28 && cms <= 32) {
             return "S";
-        } else if(cms > 38 && cms < 42) {
+        } else if(cms >32 && cms <= 38){
             return "M";
-        } else {
+        }else if(cms > 38 && cms <= 42){
             return "L";
+        }else if(cms > 42 && cms <=48 ){
+            return "XL";
+         }else{
+            return "Size not available";
         }
     }
 
     public static void main(String[] args) { 
-        assert(size(37) == "S");
-        assert(size(40) == "M");
-        assert(size(43) == "L");
-        assert(size(38) != "L");
+        assert(size(22) == "XS");
+        assert(size(32) == "S");
+        assert(size(38) == "M");
+        assert(size(42) == "L");
+        assert(size(48) == "XL");
+        assert(size(49) == "Size not available");
         System.out.println("All is well (maybe!)");
     }
 }
