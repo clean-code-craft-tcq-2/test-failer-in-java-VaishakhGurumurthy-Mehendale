88c76ff12a0700b3796cb8a7ef508df5b7d27b9b
VaishakhGurumurthy-Mehendale <91051638+VaishakhGurumurthy-Mehendale@users.noreply.github.com>
2022-02-02 01:09:42 +0530

    Comma separated values.

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,5 +1,5 @@
 public class Misaligned {
-     static String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    static String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
     static String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     static String colorMap = new String();
     static String printColorMap() {
@@ -7,8 +7,7 @@ public class Misaligned {
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
-                colorMap += i * 5 + j +" | "+majorColors[i]+" | "+minorColors[i]+"\n";
-                //System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+                colorMap += i * 5 + j +1 +","+majorColors[i]+","+minorColors[i]+"\n";
             }
         }
 
@@ -20,7 +19,7 @@ public class Misaligned {
         int result = colorMap.split("\n").length;
 
         assert(result == 25);
-        assert("1 | White | Blue" == colorMap.split("\n")[0]);
+        assert("1,White,Blue".equals(colorMap.split("\n")[0]));
 
         System.out.println("All is well (maybe!)");
     }
