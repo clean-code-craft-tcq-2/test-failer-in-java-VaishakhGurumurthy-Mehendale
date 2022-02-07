481b98bf7ab339c5ec06d438d60d4892bd97df52
VaishakhGurumurthy-Mehendale <91051638+VaishakhGurumurthy-Mehendale@users.noreply.github.com>
2022-02-07 22:24:49 +0530

    Single responsible code. Segmented test cases

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -4,35 +4,73 @@ import java.util.Random;
 
 public class Alerter {
     static int alertFailureCount = 0;
-    static boolean failureFlag;
 
+    //Stub to replicate real-time scenario: response can be anything in real-world
     static int networkAlertStub(float celcius) {
         System.out.println("ALERT: Temperature is " + celcius + " celcius");
         int random = new Random().nextInt(2);
         // assuming random network response, return some value
         return new int[]{200,500}[random];
     }
+    
+    //Stub to test fail condition
+    static int networkAlertFailStub(float celcius) {
+        return 500;
+    }
+    
+    //Stub to test pass condition
+    static int networkAlertPassStub(float celcius) {
+        return 200;
+    }
+    
+    //farenheit to celcius converter
+    static int returnCelcius(float farenheit){
+        return  (farenheit - 32) * 5 / 9;
+    }
 
-    static void alertInCelcius(float farenheit) {
-        float celcius = (farenheit - 32) * 5 / 9;
-        int returnCode = networkAlertStub(celcius);
+    
+    static void alertInCelcius(float farenheit, String testTo) {
+        float celcius = returnCelcius(farenheit);
+        int responseCode = 0;
+        
+        if(testTo == "fail"){
+            returnCode = networkAlertFailStub(celcius);
+        }else if(testTo == "pass"){
+            returnCode = networkAlertPassStub(celcius);
+        }else{
+            returnCode = networkAlertStub(celcius)
+        }
         if (returnCode != 200) {
-            failureFlag = true;
             alertFailureCount += 1;
         }
     }
+    
+    static void resetFailureCount(){
+        
+        alertFailureCount = 0;
+        
+    }
+    
+    
     public static void main(String[] args) {
+        
+        //Test to check converter
+        assert(returnCelcius(30) == 86)
 
-        // Random number of function calls to test if failureCounts are tracked properly
-        alertInCelcius(400.5f);
-        alertInCelcius(303.6f);
-
-        if(failureFlag)
-            assert (alertFailureCount > 0);
-        else
-            assert (alertFailureCount == 0);
+        // Test to check fail condition
+        alertInCelcius(400.5f, "fail");
+        assert (alertFailureCount == 1);
+        
+        alertInCelcius(303.6f, "fail");
+        assert (alertFailureCount == 2);
+        
+        resetFailureCount();
+        
+        //test to check pass condition
+        alertInCelcius(303.6f, "pass");
+        assert (alertFailureCount == 0);
+        
         System.out.printf("%d alerts failed.\n", alertFailureCount);
-
 
 
         //Test to check if networkStud returns expected set of values
