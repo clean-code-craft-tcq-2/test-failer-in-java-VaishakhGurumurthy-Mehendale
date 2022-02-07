045ae2d0df4f05f7408cd0aa13f0e02b859ced92
VaishakhGurumurthy-Mehendale <91051638+VaishakhGurumurthy-Mehendale@users.noreply.github.com>
2022-02-07 23:01:13 +0530

    Syntax update

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -12,64 +12,64 @@ public class Alerter {
         // assuming random network response, return some value
         return new int[]{200,500}[random];
     }
-    
+
     //Stub to test fail condition
     static int networkAlertFailStub(float celcius) {
         return 500;
     }
-    
+
     //Stub to test pass condition
     static int networkAlertPassStub(float celcius) {
         return 200;
     }
-    
+
     //farenheit to celcius converter
-    static int returnCelcius(float farenheit){
+    static float returnCelcius(float farenheit){
         return  (farenheit - 32) * 5 / 9;
     }
 
-    
+
     static void alertInCelcius(float farenheit, String testTo) {
         float celcius = returnCelcius(farenheit);
         int responseCode = 0;
-        
+
         if(testTo == "fail"){
-            returnCode = networkAlertFailStub(celcius);
+            responseCode = networkAlertFailStub(celcius);
         }else if(testTo == "pass"){
-            returnCode = networkAlertPassStub(celcius);
+            responseCode = networkAlertPassStub(celcius);
         }else{
-            returnCode = networkAlertStub(celcius);
+            responseCode = networkAlertStub(celcius);
         }
-        if (returnCode != 200) {
+        if (responseCode != 200) {
             alertFailureCount += 1;
         }
     }
-    
+
     static void resetFailureCount(){
-        
+
         alertFailureCount = 0;
-        
+
     }
-    
-    
+
+
     public static void main(String[] args) {
-        
+
         //Test to check converter
-        assert(returnCelcius(30) == 86);
+        assert(returnCelcius(86) == 30);
 
         // Test to check fail condition
         alertInCelcius(400.5f, "fail");
         assert (alertFailureCount == 1);
-        
+
         alertInCelcius(303.6f, "fail");
         assert (alertFailureCount == 2);
-        
+
         resetFailureCount();
-        
+
         //test to check pass condition
         alertInCelcius(303.6f, "pass");
         assert (alertFailureCount == 0);
-        
+
         System.out.printf("%d alerts failed.\n", alertFailureCount);
 
 
