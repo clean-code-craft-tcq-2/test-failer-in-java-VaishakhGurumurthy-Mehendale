2794b64f2f7ebb1fbe5ab4f30dd3d4d00609ea17
VaishakhGurumurthy-Mehendale <91051638+VaishakhGurumurthy-Mehendale@users.noreply.github.com>
2022-02-07 23:14:48 +0530

    Separate test and code

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -1,7 +1,16 @@
 import java.util.*;
-
 public class Tshirts {
-  static String size(int cms) {
+
+    static Map<String, List<Integer>> sizeMap = new HashMap<String, List<Integer>>();
+    static {
+        sizeMap.put("XS", Arrays.asList(22, 23, 24, 25, 26, 27, 28));
+        sizeMap.put("S", Arrays.asList(29, 30, 31, 32));
+        sizeMap.put("M", Arrays.asList(33, 34, 35, 36, 37, 38));
+        sizeMap.put("L", Arrays.asList(39, 40, 41, 42));
+        sizeMap.put("XL", Arrays.asList(43, 44, 45, 46, 47, 48));
+    }
+
+    static String size(int cms) {
         if(cms > 21 && cms <= 28 ) {
             return "XS";
         } else if(cms > 28 && cms <= 32) {
@@ -12,31 +21,28 @@ public class Tshirts {
             return "L";
         }else if(cms > 42 && cms <=48 ){
             return "XL";
-         }else{
+        }else{
             return "Invalid";
         }
     }
 
+
+
     public static void main(String[] args) {
 
-      Map<String, List<Integer>> sizeMap = new HashMap<String, List<Integer>>();
-      sizeMap.put("XS", Arrays.asList(22, 23, 24, 25, 26, 27, 28));
-      sizeMap.put("S", Arrays.asList(29, 30, 31, 32));
-      sizeMap.put("M", Arrays.asList(33, 34, 35, 36, 37, 38));
-      sizeMap.put("L", Arrays.asList(39, 40, 41, 42));
-      sizeMap.put("XL", Arrays.asList(43, 44, 45, 46, 47, 48));
 
-      //Test to cover all possible test values
-      for(int textSize=0; textSize<100; textSize++){
+        //Test to cover all possible test values
+        for(int textSize=0; textSize<100; textSize++){
 
-          if(textSize<22 || textSize>48){
-              assert(size(textSize).equals("Invalid"));
-          }else {
-              assert (sizeMap.get(size(textSize)).contains(textSize));
-          }
+            if(textSize<22 || textSize>48){
+                assert(size(textSize).equals("Invalid"));
+            }else {
+                assert (sizeMap.get(size(textSize)).contains(textSize));
+            }
 
-      }
+        }
 
         System.out.println("All is well (maybe!)");
     }
+
 }
